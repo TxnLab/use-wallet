f3ac34c7e052a0f438e842f1db2fc6c99cffa6e5
Gabriel Kuettel <gabrielkuettel@gmail.com>
2022-10-21 13:37:26 -0500

    feat: add env vars to set nodes

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -6,6 +6,13 @@ export enum PROVIDER_ID {
   EXODUS = "Exodus",
 }
 
-export const NODE_SERVER = "https://mainnet-api.algonode.cloud";
-export const NODE_TOKEN = "";
-export const NODE_PORT = "";
+export const NODE_SERVER =
+  process.env.REACT_APP_NODE_URL ||
+  process.env.NEXT_PUBLIC_NODE_URL ||
+  "https://mainnet-api.algonode.cloud";
+
+export const NODE_TOKEN =
+  process.env.REACT_APP_NODE_TOKEN || process.env.NEXT_PUBLIC_NODE_TOKEN || "";
+
+export const NODE_PORT =
+  process.env.REACT_APP_NODE_PORT || process.env.NEXT_PUBLIC_NODE_PORT || "";
