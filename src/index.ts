fc553abebc3bb0273f918491fb3776465fa33a5f
Gabriel Kuettel <gabrielkuettel@gmail.com>
2022-10-21 13:36:55 -0500

    feat: export utils, clients

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
 export { useWallet, useConnectWallet } from "./hooks";
 export * from "./constants";
 export * from "./types";
+export * from "./utils";
+export * from "./providers";
+export * from "./clients";
