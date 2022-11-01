a3ed3f095e7d876aad25168fc8d664b6d07094c1
Gabriel Kuettel <gabrielkuettel@gmail.com>
2022-11-01 10:10:23 -0500

    feat: export hooks and constants from main entry point

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,2 @@
-export { useWallet, useConnectWallet } from "./hooks";
 export * from "./constants";
-export * from "./types";
-export * from "./utils";
-export * from "./providers";
-export * from "./clients";
+export * from "./hooks";
