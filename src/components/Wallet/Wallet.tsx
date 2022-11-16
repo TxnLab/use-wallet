bc707f82cfa1cfc149bcd9cf7bd56a466418dcf7
Gabriel Kuettel <gabrielkuettel@gmail.com>
2022-11-16 12:10:09 -0600

    feat: use variable for readability
    Co-authored-by: Doug Richar <drichar@gmail.com>

--- src/components/Wallet/Wallet.tsx
+++ src/components/Wallet/Wallet.tsx
@@ -40,7 +40,8 @@ export default function Wallet(props: WalletProps) {
     const encodedTransaction = algosdk.encodeUnsignedTransaction(transaction);
     const signedTransactions = await signTransactions([encodedTransaction]);
 
-    const { id } = await sendTransactions(signedTransactions, 4);
+    const waitRoundsToConfirm = 4
+    const { id } = await sendTransactions(signedTransactions, waitRoundsToConfirm);
 
     console.log("Successfully sent transaction. Transaction ID: ", id);
   };
