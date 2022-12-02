ad11ff54ae38b025aff9e6523849b09b5cca3d00
Gabriel Kuettel <gabrielkuettel@gmail.com>
2022-12-01 22:03:43 -0700

    feat: log info instead of error

--- src/clients/defly/client.ts
+++ src/clients/defly/client.ts
@@ -83,7 +83,7 @@ class DeflyWalletClient extends BaseWallet {
   }
 
   async reconnect(onDisconnect: () => void) {
-    const accounts = await this.#client.reconnectSession().catch(console.error);
+    const accounts = await this.#client.reconnectSession().catch(console.info);
     this.#client.connector?.on("disconnect", onDisconnect);
 
     if (!accounts) {
