0f1f23fb2d1de692c989f2429bd673ce89992bd4
Doug Richar <drichar@gmail.com>
2023-06-26 05:20:33 -0400

    chore(example): update Storybook example to useInitializeProviders

--- src/components/Example/Example.tsx
+++ src/components/Example/Example.tsx
@@ -1,26 +1,21 @@
-import React, { useState, useEffect } from 'react'
-import {
-  reconnectProviders,
-  initializeProviders,
-  WalletProvider,
-  SupportedProviders
-} from '../../index'
+import React from 'react'
+import { DeflyWalletConnect } from '@blockshake/defly-connect'
+import { PeraWalletConnect } from '@perawallet/connect'
+import { DaffiWalletConnect } from '@daffiwallet/connect'
+import { WalletProvider, PROVIDER_ID, useInitializeProviders } from '../../index'
 import Account from './Account'
 import Connect from './Connect'
 import Transact from './Transact'
 
 export default function ConnectWallet() {
-  const [walletProviders, setWalletProviders] = useState<SupportedProviders | null>(null)
-
-  useEffect(() => {
-    async function initializeAndConnect() {
-      const providers = await initializeProviders()
-      setWalletProviders(providers)
-      reconnectProviders(providers)
-    }
-
-    initializeAndConnect()
-  }, [])
+  const walletProviders = useInitializeProviders({
+    providers: [
+      { id: PROVIDER_ID.DEFLY, clientStatic: DeflyWalletConnect },
+      { id: PROVIDER_ID.PERA, clientStatic: PeraWalletConnect },
+      { id: PROVIDER_ID.DAFFI, clientStatic: DaffiWalletConnect },
+      { id: PROVIDER_ID.EXODUS }
+    ]
+  })
 
   return (
     <WalletProvider value={walletProviders}>
