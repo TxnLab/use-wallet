569c5878f363ecbf131e99ba8fbb604747648837
Doug Richar <drichar@gmail.com>
2023-12-15 00:21:59 -0500

    feat: useWallet throws error if used outside WalletProvider
    This also renames the context provider to match the exported "WalletProvider", and the context object itself is imported via a `useWalletContext` hook.

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,3 +1,2 @@
 export * from './state/walletStore'
 export * from './state/debugStore'
-export { default as WalletProvider } from './state/clientStore'
--- src/store/state/clientStore.ts
+++
@@ -1,8 +0,0 @@
-import { createContext } from 'react'
-import { SupportedProviders } from '../../types/providers'
-
-const ClientContext = createContext<SupportedProviders | null>(null)
-
-export { ClientContext }
-
-export default ClientContext.Provider
