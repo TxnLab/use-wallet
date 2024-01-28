a57079d407c72188c299b1489d3b82481a6b8dc5
Altynbek Orumbayev <aorumbayev@protonmail.com>
2024-01-28 01:50:48 +0100

    chore: minor tweaks

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
-  plugins: ['@babel/plugin-proposal-class-properties']
+  "plugins": [["@babel/plugin-transform-class-properties", { "loose": true }]]
 }
