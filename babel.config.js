e8e79df141c3ece3f0ac2439e8735bd2024d4e02
Altynbek Orumbayev <aorumbayev@protonmail.com>
2024-01-28 01:56:53 +0100

    chore: prettier tweaks

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
-  "plugins": [["@babel/plugin-transform-class-properties", { "loose": true }]]
+  plugins: [['@babel/plugin-transform-class-properties', { loose: true }]]
 }
