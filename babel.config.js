c01677f75c651199768adfe0cb506551820ee369
Doug Richar <drichar@gmail.com>
2024-01-30 15:22:43 -0500

    chore(storybook): update Babel config to suppress warning
    See https://github.com/rails/webpacker/issues/3008#issuecomment-895722363

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,8 @@
 module.exports = {
   presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
-  plugins: [['@babel/plugin-transform-class-properties', { loose: true }]]
+  plugins: [
+    ['@babel/plugin-transform-class-properties', { loose: true }],
+    ['@babel/plugin-proposal-private-methods', { loose: true }],
+    ['@babel/plugin-proposal-private-property-in-object', { loose: true }]
+  ]
 }
