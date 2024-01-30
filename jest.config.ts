9ce4bb3f3e144f779ae636aa1abf86310b065ef2
Doug Richar <drichar@gmail.com>
2024-01-30 15:14:58 -0500

    chore(jest): update test environment to jest-environment-jsdom

--- jest.config.ts
+++ jest.config.ts
@@ -146,7 +146,7 @@ export default {
   // snapshotSerializers: [],
 
   // The test environment that will be used for testing
-  testEnvironment: 'jest-environment-node'
+  testEnvironment: 'jest-environment-jsdom'
 
   // Options that will be passed to the testEnvironment
   // testEnvironmentOptions: {},
