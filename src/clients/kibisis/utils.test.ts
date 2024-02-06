e686564f09e166fef9b21fdec117884934d7383a
Kieran O'Neill <kieran.oneill@pm.me>
2024-02-07 00:08:26 +0200

    fix(kibisis): replace global object with window (#139)

--- src/clients/kibisis/utils.test.ts
+++ src/clients/kibisis/utils.test.ts
@@ -1,9 +1,19 @@
 import { generateUuid } from './utils'
-import { randomUUID, getRandomValues } from 'crypto'
+import { getRandomValues, randomUUID } from 'crypto'
 
 describe(`${__dirname}/utils`, () => {
   const validUuidRegex =
     /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i
+
+  beforeAll(() => {
+    Object.defineProperty(window, 'crypto', {
+      configurable: true,
+      value: {
+        getRandomValues,
+        randomUUID
+      }
+    })
+  })
 
   describe('generateUuid()', () => {
     it('should generate a valid uuid using the web crypto api', () => {
@@ -13,23 +23,15 @@ describe(`${__dirname}/utils`, () => {
     })
 
     it('should generate a valid uuid without the web crypto api', () => {
-      Object.defineProperty(global, 'crypto', {
-        configurable: true,
-        value: {
-          getRandomValues
-        }
-      })
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      delete window.crypto.randomUUID
 
       const result = generateUuid()
 
       expect(validUuidRegex.test(result)).toBe(true)
 
-      Object.defineProperty(global, 'crypto', {
-        value: {
-          getRandomValues,
-          randomUUID
-        }
-      })
+      window.crypto.randomUUID = randomUUID
     })
   })
 })
