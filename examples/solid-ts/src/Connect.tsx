c36c7a7a767ddcc8c81da3d64b37811d359c19cc
Doug Richar <drichar@gmail.com>
2024-05-16 03:19:49 -0400

    chore: match Solid.js example app UI to others

--- examples/solid-ts/src/Connect.tsx
+++ examples/solid-ts/src/Connect.tsx
@@ -1,30 +1,14 @@
-import { NetworkId, useWallet } from '@txnlab/use-wallet-solid'
+import { useWallet } from '@txnlab/use-wallet-solid'
 import { For, Show } from 'solid-js'
-import encoding from 'algosdk'
 
 export function Connect() {
-  const {
-    activeAccount,
-    activeAddress,
-    activeNetwork,
-    activeWallet,
-    activeWalletAccounts,
-    activeWalletAddresses,
-    activeWalletId,
-    activeWalletState,
-    algodClient,
-    isWalletActive,
-    isWalletConnected,
-    setActiveNetwork,
-    wallets
-  } = useWallet()
+  const { activeWalletId, isWalletActive, isWalletConnected, wallets } = useWallet()
 
   return (
     <div>
       <For each={wallets}>
         {(wallet) => (
           <div>
-            <p>{wallet.id}</p>
             <h4>
               {wallet.metadata.name}{' '}
               <Show when={wallet.id === activeWalletId()} fallback="">
@@ -67,31 +51,11 @@ export function Connect() {
                     {(account) => <option value={account.address}>{account.address}</option>}
                   </For>
                 </select>
-                <p>wallet.id: {wallet.id}</p>
-                <p>wallet.metadata: {wallet.metadata.name}</p>
-                <span>wallet.icon: </span>
-                <img src={wallet.metadata.icon} height={24} width={24} />
-                <p>wallet.activeAccount: {wallet.activeAccount?.name}</p>
-                <p>wallet.accounts: {JSON.stringify(wallet.accounts)}</p>
               </div>
             </Show>
           </div>
         )}
       </For>
-      <button onClick={() => setActiveNetwork(NetworkId.MAINNET)}>Set Mainnet</button>
-      <button onClick={() => setActiveNetwork(NetworkId.TESTNET)}>Set Testnet</button>
-      <p>activeNetwork: {activeNetwork().toString()}</p>
-      <p>activeWalletId: {activeWalletId()}</p>
-      <p>activeWallet: {activeWallet()?.metadata.name}</p>
-      <p>activeWalletAccounts: {JSON.stringify(activeWalletAccounts())}</p>
-      <p>activeWalletAddresses: {activeWalletAddresses()?.join(', ')}</p>
-      <p>activeWalletState: {JSON.stringify(activeWalletState())}</p>
-      <p>activeAccount: {JSON.stringify(activeAccount())}</p>
-      <p>activeAddress: {activeAddress()}</p>
-      <p>algodClient int encoding: {algodClient().getIntEncoding()}</p>
-      <button onClick={() => algodClient().setIntEncoding(encoding.IntDecoding.SAFE)}>
-        Set Int encoding
-      </button>
     </div>
   )
 }
