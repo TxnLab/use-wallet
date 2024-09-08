e02e28c58e92f5b26084e88d9b819c7969f19180
Ludovit Scholtz <ludko2@gmail.com>
2024-09-08 16:24:35 +0200

    prettier fix

--- packages/use-wallet/src/wallets/kibisis.ts
+++ packages/use-wallet/src/wallets/kibisis.ts
@@ -210,13 +210,13 @@ export class KibisisWallet extends BaseWallet {
 
       const module = await import('@agoralabs-sh/avm-web-provider')
       this.avmWebProviderSDK = module.default ? module.default : module
-      
+
       if (!this.avmWebProviderSDK) {
         throw new Error(
           'failed to initialize, the @agoralabs-sh/avm-web-provider sdk was not provided'
         )
       }
-      
+
       if (!this.avmWebProviderSDK.AVMWebClient) {
         throw new Error(
           'failed to initialize, the @agoralabs-sh/avm-web-provider sdk was not provided. AVMWebClient missing'
