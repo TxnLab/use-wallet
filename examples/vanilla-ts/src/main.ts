15f9c3a106d680873c151d713aa854d154c4faab
Doug Richar <drichar@gmail.com>
2024-10-21 14:21:24 -0400

    refactor(examples): simplify LiquidWallet configuration
    Update example apps to use enum syntax for `LiquidWallet` when no
    custom options are needed.

--- examples/vanilla-ts/src/main.ts
+++ examples/vanilla-ts/src/main.ts
@@ -20,9 +20,7 @@ const walletManager = new WalletManager({
     },
     WalletId.KMD,
     WalletId.KIBISIS,
-    {
-      id: WalletId.LIQUID
-    },
+    WalletId.LIQUID,
     {
       id: WalletId.LUTE,
       options: { siteName: 'Example Site' }
