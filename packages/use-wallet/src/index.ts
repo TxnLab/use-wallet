5d872d2d05c8dad2b43a2b142d5a41129efe47c3
Doug Richar <drichar@gmail.com>
2025-01-30 12:41:02 -0500

    fix(core): export NetworkConfigBuilder class
    The `NetworkConfigBuilder` class, a key component of the new network
    configuration API, was missing from the main exports. This class enables
    users to customize network configurations with a fluent builder pattern.

--- packages/use-wallet/src/index.ts
+++ packages/use-wallet/src/index.ts
@@ -1,6 +1,12 @@
 export { LogLevel } from './logger'
 export { WalletManager, WalletManagerConfig, WalletManagerOptions } from './manager'
-export { AlgodConfig, NetworkConfig, NetworkId, DEFAULT_NETWORK_CONFIG } from './network'
+export {
+  AlgodConfig,
+  NetworkConfig,
+  NetworkConfigBuilder,
+  NetworkId,
+  DEFAULT_NETWORK_CONFIG
+} from './network'
 export { State, WalletState, ManagerStatus, DEFAULT_STATE } from './store'
 export { StorageAdapter } from './storage'
 export { webpackFallback } from './webpack'
