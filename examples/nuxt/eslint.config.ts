bff078244f220baffdf319abd986ddb4aaf4afe2
Andy Funk <acfunk@gmail.com>
2025-05-05 16:40:32 -0600

    revert @types/node version; add eslint to vue examples

---
+++ examples/nuxt/eslint.config.ts
@@ -0,0 +1,24 @@
+import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
+import pluginVue from 'eslint-plugin-vue'
+
+// To allow more languages other than `ts` in `.vue` files, uncomment the following lines:
+// import { configureVueProject } from '@vue/eslint-config-typescript'
+// configureVueProject({ scriptLangs: ['ts', 'tsx'] })
+// More info at https://github.com/vuejs/eslint-config-typescript/#advanced-setup
+
+export default defineConfigWithVueTs(
+  {
+    name: 'app/files-to-lint',
+    files: ['**/*.{ts,mts,tsx,vue}']
+  },
+
+  pluginVue.configs['flat/essential'],
+  vueTsConfigs.recommended,
+
+  {
+    rules: {
+      'vue/multi-word-component-names': 'off',
+      'vue/block-lang': 'off'
+    }
+  }
+)
