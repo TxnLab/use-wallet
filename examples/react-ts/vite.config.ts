1ca859d6f265df5b0d2e4348647e13d15379442e
Andrew Funk <acfunk@gmail.com>
2025-06-17 14:03:46 -0600

    fix: node polyfills

--- examples/react-ts/vite.config.ts
+++ examples/react-ts/vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), nodePolyfills()],
   build: {
     chunkSizeWarningLimit: 700
   }
