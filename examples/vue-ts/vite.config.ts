e932dec5d42959cd1f3c3d545f9485e99f85e3a9
Andrew Funk <acfunk@gmail.com>
2025-06-18 16:36:29 -0600

    fix(examples): add node polyfills and set framework favicons (#374)
    * fix: node polyfills
    
    * add vite-plugin-node-polyfills to root

--- examples/vue-ts/vite.config.ts
+++ examples/vue-ts/vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), nodePolyfills()],
   build: {
     chunkSizeWarningLimit: 600
   }
