a96aa9fb8b97a50af9c7d2eb0fe97a341f0f06c8
Doug Richar <drichar@gmail.com>
2026-09-20 15:00:17 -0400

    chore: post-Lute-removal cleanup, algosdk Renovate grouping, tsdown 0.23 typecheck prep

--- examples/next/next.config.ts
+++ examples/next/next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from 'next'
 
-const nextConfig: NextConfig = {
-  /* config options here */
-}
+// Add config options here
+const nextConfig: NextConfig = {}
 
 export default nextConfig
